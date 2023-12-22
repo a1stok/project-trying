c654ee1b8da3c6b1091a6f9034a5fe81e7a84b9d
a1stok <kostianvs13@gmail.com>
2023-12-22 01:17:36 -0500

    creted js for movies:phase 1

---
+++ script.js
@@ -0,0 +1,24 @@
+const numberOfFilms = +prompt("how many movies have you watched?","");
+const personalMovieDB = {
+     count : numberOfFilms,
+     movies : {
+
+    },
+     actors : {
+
+    },
+     genres : [ ],
+     privat : false 
+}
+const a = prompt("One of the last seen movies?"," ");
+const b = +prompt("how would you rate it?"," ");
+
+const d = prompt("One of the last seen movies?"," ");
+const c = +prompt("how would you rate it?"," ");
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[d]=c;
+
+console.log(personalMovieDB);
+console.log(numberOfFilms);
+alert(numberOfFilms);
