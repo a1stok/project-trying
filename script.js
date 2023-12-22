0c87891607dc7542626eb6fe00b67188fc1e053b
a1stok <kostianvs13@gmail.com>
2023-12-22 01:27:55 -0500

    pls

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const numberOfFilms = +prompt("how many movies have you watched?","");
 const personalMovieDB = {
      count : numberOfFilms,
@@ -17,14 +16,6 @@ const b = +prompt("how would you rate it?"," ");
 const d = prompt("One of the last seen movies?"," ");
 const c = +prompt("how would you rate it?"," ");
 
+
 personalMovieDB.movies[a]=b;
 personalMovieDB.movies[d]=c;
-
-console.log(personalMovieDB);
-console.log(numberOfFilms);
-alert(numberOfFilms);
-=======
-"use strict";
-const rsult =confirm("did you do it?");
-console.log(result);
->>>>>>> 9ac898fd1b651d9219921f62aab4b00730b361b1
