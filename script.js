8d1af48b2e19bd9b03e062a0040b36e12059b000
a1stok <kostianvs13@gmail.com>
2023-12-23 23:22:07 -0500

    created loops and used fucntions for movie project.phase 2

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const numberOfFilms = +prompt("how many movies have you watched?","");
+let numberOfFilms;
+function start(){
+     numberOfFilms = +prompt("how many movies have you watched?","");
+
+     while(numberOfFilms =="" || numberOfFilms==null || isNaN(numberOfFilms)){
+     numberOfFilms = +prompt("how many movies have you watched?","");}
+     }
+     start();
+
+
+
 const personalMovieDB = {
+
+
+
+
      count : numberOfFilms,
      movies : {
 
@@ -8,14 +22,55 @@ const personalMovieDB = {
 
     },
      genres : [ ],
-     privat : false 
+     privat : false
+}    
+function remembermyFilms() {
+     for (let i=0;i<2;i++){
+          const a = prompt("One of the last seen movies?"," ");
+          const b = +prompt("how would you rate it?"," ");
+     if (a != null && b != null && a!="" & b !=""&& a.length<50){
+          personalMovieDB.movies[a]=b; 
+          console.log("Done");}
+     else {
+          console.log("error");
+          i--;
+     }
 }
-const a = prompt("One of the last seen movies?"," ");
-const b = +prompt("how would you rate it?"," ");
+}
+remembermyFilms();
 
-const d = prompt("One of the last seen movies?"," ");
-const c = +prompt("how would you rate it?"," ");
+function detectPersonalLevel(){
+     if(personalMovieDB.count<10){
+          console.log("You have not seen much films");
+     }
+     else if(personalMovieDB.count>=10&&personalMovieDB.count<30){
+     console.log("you are a good viewer");}
+     else if(personalMovieDB.count>=30){
+          console.log("you are a cinephile ");}
+     else{
+          console.log("error occured");}
+     
+     
+}
+function showMyDB(hidden){
+     if(!hidden){
+          console.log(personalMovieDB);
+     }
+}
+showMyDB(personalMovieDB.privat);
 
 
-personalMovieDB.movies[a]=b;
-personalMovieDB.movies[d]=c;
+ function writeYourGenres(){
+     for(let  i=1;i<=3;i++){
+          a=prompt(`What is your num ${i}genre?`,"");
+          if(a.length<5||a===""||a==null){
+               console.log("That`s invalid genre!");
+               i--;
+          }
+          else{
+          personalMovieDB.genres[i-1]=a;}
+
+
+     }
+ }
+ writeYourGenres();
